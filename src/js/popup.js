947bc279bebb263e2eae7208838a196b520255c7
Nekitech <beliaew.nikita@mail.ru>
2022-12-17 12:50:41 +0300

    popups is done, afterSendForm is done

--- src/js/popup.js
+++ src/js/popup.js
@@ -2,8 +2,8 @@ class Popup {
     constructor(listId) {
         this.listId = listId;
     }
+
     init() {
-        console.log(this.listId)
         this.listId.forEach((btn) => {
             btn.addEventListener('click', (e) => {
                 const id_popup = e.target?.getAttribute('id')
@@ -20,6 +20,7 @@ class Popup {
             })
         })
     }
+
     open(popup) {
         popup.classList.add('activePopup')
         document.body.style.overflowY = 'hidden'
@@ -30,6 +31,8 @@ class Popup {
         popup.classList.remove('activePopup')
         document.body.style.overflowY = 'auto'
         popup_fon.style.display = 'none'
+
+        resetSendForm(popup, true)
     }
 }
 
@@ -38,6 +41,7 @@ const id_popupForm = document.querySelectorAll('.QandA__popularQuestions__button
 
 const popup_fon = document.querySelector('.popup-fon')
 const close_popupsCards = document.querySelectorAll('.popup-close')
+
 
 const popupCardsInst = new Popup(id_popupsCards)
 popupCardsInst.init()
@@ -50,4 +54,23 @@ popup_fon.addEventListener('click', (e) => {
     popup.classList.remove('activePopup')
     document.body.style.overflowY = 'auto'
     popup_fon.style.display = 'none'
+
+    resetSendForm(popup, true)
 })
+
+const sendFormBtn = document.querySelector('.popupForm__button');
+
+
+sendFormBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    resetSendForm(document.querySelector('.popupForm'), false)
+})
+
+const resetSendForm = (popup, turn) => {
+    if (!popup.classList.contains('popupForm')) return
+
+    const popupFormWrapper = popup.querySelector('.popupForm__wrapper');
+    const afterSendForm = popup.querySelector('.popupForm__afterSendForm');
+    popupFormWrapper.style.display = (turn) ? 'block' : 'none'
+    afterSendForm.style.display = (turn) ? 'none' : 'flex'
+}
